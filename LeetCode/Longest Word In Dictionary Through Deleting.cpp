797edbb62322c1baa1745c4fc3ae01cffd20806f
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-12-09 12:46:12 +0530

    easy

--- LeetCode/Longest Word In Dictionary Through Deleting.cpp
+++ LeetCode/Longest Word In Dictionary Through Deleting.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == t[j])
+                j++;
+        }
+        return (j == t.size());
+    }
+    
     string findLongestWord(string s, vector<string>& d) {
         string ans;
-        for(string t : d) {
-            int i = 0, j = 0;
-            for(j = 0; j < s.size(); j++) {
-                if(s[j] == t[i])
-                    i++;
-            }
-            if(i == t.size() && (t.size() > ans.size() || (t.size() == ans.size() && t < ans))) {
-                ans = t;
+        for(int i = 0; i < d.size(); i++) {
+            if(isSubsequence(s, d[i])) {
+                if(ans == "" || d[i].size() > ans.size())   
+                    ans = d[i];
+                else if(d[i].size() == ans.size())   
+                    ans = min(ans, d[i]);
             }
         }
         return ans;
