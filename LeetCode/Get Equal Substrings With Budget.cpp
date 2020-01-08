26f0a9b59a2dad8a9e621b1f40f27685da78e262
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2020-01-08 22:57:41 +0530

    easy

--- LeetCode/Get Equal Substrings With Budget.cpp
+++ LeetCode/Get Equal Substrings With Budget.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int k) {
-        int ret = 0, i = 0, j = 0;
-        while(j < s.size()) {
-            if((k -= abs(s[j] - t[j++])) < 0)
-                k += abs(s[i] - t[i++]);
-            ret = max(ret, j - i);
+    int equalSubstring(string s, string t, int maxCost) {
+        int maxLen = 0, i = 0, cost = 0;
+        for(int j = 0; j < s.size(); j++) {
+            cost += abs(s[j] - t[j]);
+            while(cost > maxCost && i < s.size()) {
+                cost -= abs(s[i] - t[i]);
+                i++;
+            }
+            maxLen = max(maxLen, j - i + 1);
         }
-        return ret;
+        return maxLen;
     }
 };
